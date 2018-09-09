623be20ecf57f00c1a3baf519b08da52a25b512d
YennyCastillo <aricrisf@hotmail.com>
2018-09-09 15:35:46 -0500

    iniciando portafolio

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    M.AutoInit();
+ });
+      ;
+   //carousel
+  document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.slider');
+   var instances = M.Slider.init(elems, {
+     indicators: false,
+     interval: 2000,
+     height: 500,
+   });
+ });  
