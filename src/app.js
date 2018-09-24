e90539d652a84738f864d34ad47fff552c09e5c0
YennyCastillo <aricrisf@hotmail.com>
2018-09-24 01:30:47 -0500

    actualizando

--- src/app.js
+++ src/app.js
@@ -1,19 +1,9 @@
+
 $(document).ready(function(){
-    M.AutoInit();
- });
-      ;
-   //carousel
-  
- document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.parallax');
-  var instances = M.Parallax.init(elems,{
-    responsiveThreshold: 0,	
-  });
+  $('.parallax').parallax();
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.slider');
-  var instances = M.Slider.init(elems,{
-    height: 350,
-  });
+$(document).ready(function(){
+  $('.slider').slider();
 });
+      
